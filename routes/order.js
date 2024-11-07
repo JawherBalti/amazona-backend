a4716a04dbcc4206274cc2d38c4a419f94b9333f
jawherbalti <jawher-balti@live.com>
2024-11-07 09:31:56 +0100

    separate backend from frontend

---
+++ routes/order.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const orderController = require("../controller/order");
+const { isAuth, isAdmin } = require("../middlewares/auth");
+
+const orderRoute = express.Router();
+
+orderRoute.post("/", isAuth, orderController.placeOrder);
+orderRoute.get("/myorder", isAuth, orderController.myOrderList); //put this before getOrder route otherwise "myorder" will be considered as :id and getOrder will be called instead
+orderRoute.get("/orders", isAuth, orderController.getOrders);
+orderRoute.get("/summary", isAuth, isAdmin, orderController.getSummary);
+orderRoute.get("/:id", orderController.getOrder);
+orderRoute.put("/pay/:id", isAuth, orderController.updateOrder);
+orderRoute.put("/deliver/:id", isAuth, orderController.deliverOrder);
+orderRoute.delete("/:id", isAuth, orderController.deleteOrder);
+
+module.exports = orderRoute;
