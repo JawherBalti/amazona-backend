3b1cbf26df3c3e25100f6622a87e09d57ddf9cc6
jawherbalti <jawher-balti@live.com>
2024-11-07 09:34:31 +0100

    get dashboard summary

--- controller/order.js
+++ controller/order.js
@@ -90,6 +90,45 @@ const deleteOrder = (req, res) => {
     );
 };
 
+const getSummary = async (req, res) => {
+  const orders = await Order.aggregate([
+    {
+      $group: {
+        _id: null,
+        numOrders: { $sum: 1 },
+        totalSales: { $sum: "$totalPrice" },
+      },
+    },
+  ]);
+  const users = await User.aggregate([
+    {
+      $group: {
+        _id: null,
+        numUsers: { $sum: 1 },
+      },
+    },
+  ]);
+  const dailyOrders = await Order.aggregate([
+    {
+      $group: {
+        _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+        orders: { $sum: 1 },
+        sales: { $sum: "$totalPrice" },
+      },
+    },
+    { $sort: { _id: 1 } },
+  ]);
+  const productCategories = await Product.aggregate([
+    {
+      $group: {
+        _id: "$category",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+  res.send({ users, orders, dailyOrders, productCategories });
+};
+
 module.exports = {
   placeOrder,
   getOrders,
@@ -98,4 +137,5 @@ module.exports = {
   myOrderList,
   deleteOrder,
   deliverOrder,
+  getSummary
 };
