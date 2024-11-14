b78327f9e7d3fee7932f24b7476f7d6355252c44
jawherbalti <jawher-balti@live.com>
2024-11-14 18:42:16 +0100

    add frontend url to cors origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: ['http://localhost:3000'], // Allow requests from this origin
+    origin: ['https://amazona-client.vercel.app'], // Allow requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
