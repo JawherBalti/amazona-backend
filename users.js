63b936cabc5c0f94345b7ba2315292abf783f2f3
jawherbalti <jawher-balti@live.com>
2024-11-14 18:01:56 +0100

    data to seed users and products

---
+++ users.js
@@ -0,0 +1,54 @@
+const usersData = {
+  users: [
+    {
+      _id: "6728dc6851028a263c6aaf15",
+      isAdmin: true,
+      name: "jawhar",
+      email: "jawher94@gmail.com",
+      password: "$2b$10$shMwaYyKykGvMSWv3Tt65ulY5Hiq0U70DTyiB4B.BLgbHLEcrTkd6",
+    },
+    {
+      _id: "6728f143f2372e2f188d35b4",
+      isAdmin: false,
+      name: "jok",
+      email: "jawher-balti@live.com",
+      password: "$2b$10$0v2qL7V0/9YbgnhWWf4WUOOC1ELb0Vyz9ArmnXNSrtRKgkY/je1..",
+    },
+    {
+      _id: "672c8b026721580dd8e59a78",
+      isAdmin: false,
+      name: "angelo collier",
+      email: "eusebio_balistreri@yahoo.com",
+      password: "$2b$10$YCWi0gUmZ4ZAkABKzCgEO.6NTDuzYwGUBIWh5c.Cz.9g.TUbm1oxy",
+    },
+    {
+      _id: "672c8b026721580dd8e59a79",
+      isAdmin: false,
+      name: "lori zulauf",
+      email: "mohammad_terry47@hotmail.com",
+      password: "$2b$10$CSi3WHGHw4W5liH42zhXLOF/JKjU8FeMkIYstj52k3bqfOOJTyMey",
+    },
+    {
+      _id: "672c8b036721580dd8e59a7a",
+      isAdmin: false,
+      name: "guy harris phd",
+      email: "vladimir_johnston@hotmail.com",
+      password: "$2b$10$I.NzjE7riYfzUv.3QmP4nOAgOWJv6w8arBS/lDz1e75y/fJFIlLNa",
+    },
+    {
+      _id: "672c8b036721580dd8e59a7b",
+      isAdmin: false,
+      name: "sidney jones md",
+      email: "collin.gleason72@yahoo.com",
+      password: "$2b$10$wNH8yEpzN8wyn6ZlTYjK9uae6UIgr.h42agz3LvScAQlY.T7D3X1m",
+    },
+    {
+      _id: "672c8b036721580dd8e59a7c",
+      isAdmin: false,
+      name: "dr. desiree zboncak",
+      email: "hal88@gmail.com",
+      password: "$2b$10$MJc5UtxT1qZKO2Zy5E0/8OVsnOWSNv4LZPVNJ30aXj0HyjcQMFAhG",
+    },
+  ],
+};
+module.exports = usersData
