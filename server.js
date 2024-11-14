bd52ee1dbae28d97385fe1a7b920b8b6ea86c016
jawherbalti <jawher-balti@live.com>
2024-11-14 18:46:58 +0100

    allow all origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: ['https://amazona-client.vercel.app'], // Allow requests from this origin
+    origin: ['*'], // Allow requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
